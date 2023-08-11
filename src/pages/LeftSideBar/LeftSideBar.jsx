332afb175fe40b5b81fccec6ae11f3afa1ace451
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-08-11 16:22:25 +0600

    final

--- src/pages/LeftSideBar/LeftSideBar.jsx
+++ src/pages/LeftSideBar/LeftSideBar.jsx
@@ -6,10 +6,10 @@ const LeftSideBar = () => {
             <div className="h-96">
                 <img title="Masum Billah" className="p-5 w-20 h-20 rounded-full" src="profileImge.jpg" alt="" />
                 <h2 className="text-white">Masum Billah</h2>
-                <img title="Masum Billah" className="p-5 w-20 h-20 rounded-full" src="profileImge.jpg" alt="" />
+                <img title="Masum Billah" className="p-5 w-20 h-20 rounded-full" src="male.png" alt="" />
                 <h2 className="text-white">John Doe</h2>
-                <img title="Masum Billah" className="p-5 w-20 h-20 rounded-full" src="profileImge.jpg" alt="" />
-                <h2 className="text-white">Jenifer</h2>
+                <img title="Masum Billah" className="p-5 w-20 h-20 rounded-full" src="female.png" alt="" />
+                <h2 className="text-white">Emy Jackson</h2>
                 
                 
             </div>
